c47e9faaef7aa6c1c82725627d48085d226f4429
Kiyoshi Yagi <kiyoshi.yagi@riken.jp>
2017-11-11 16:47:03 +0900

    added mpi parallel job for vib

--- g09/wako_cluster/submit_openMPI_GridEngine.sh
+++ g09/wako_cluster/submit_openMPI_GridEngine.sh
@@ -9,15 +9,18 @@
 #$ -cwd
 #$ -N test_min
 #$ -j y
-#$ -pe ompi 12
-#$ -q kushana.q
+#$ -pe ompi 16
+#$ -q y2.q
 ######################################################
 #
 
-GENESIS=/home/yagi/devel/genesis/genesis.gat_qmmm_diva/bin/atdyn
+GENESIS=/home/yagi/devel/genesis/genesis.gat_qmmm_beluga/bin/atdyn
 
-export OMP_NUM_THREADS=12
-mpirun -machinefile $TMP/machines -np 1 --map-by slot:pe=12 $GENESIS genesis_qmmm.inp >& genesis_qmmm.out
+export OMP_NUM_THREADS=16
+mpirun -machinefile $TMP/machines -np 1 --map-by slot:pe=16 $GENESIS genesis_qmmm.inp >& genesis_qmmm.out
+
+export OMP_NUM_THREADS=4
+mpirun -machinefile $TMP/machines -np 4 --map-by slot:pe=4 $GENESIS genesis_qmmm_vib.inp >& genesis_qmmm_vib.out
 
 exit 0
 
