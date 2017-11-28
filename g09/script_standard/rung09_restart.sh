2061e8ecbfd96b5d45115f6918cd32537e212176
Kiyoshi Yagi <kiyoshi.yagi@riken.jp>
2017-11-29 02:17:50 +0900

    bugfix restart

--- g09/script_standard/rung09_restart.sh
+++ g09/script_standard/rung09_restart.sh
@@ -4,6 +4,10 @@
 # restart from given log and Fchk file
 # -----------------------------------------------
 
+QMINP=$1
+QMOUT=$2
+MOL=${QMINP%.*}
+
 if [ -e ${MOL}.Fchk ]; then
    cp ${MOL}.Fchk gaussian.Fchk
    exit 0
