c9ac3749c115f1a425fd9d91252a16e56aaaee13
Kiyoshi Yagi <kiyoshi.yagi@riken.jp>
2024-02-17 14:01:49 +0900

    add restart file

--- examples/Ala3/1_dftb+/script.sh
+++ examples/Ala3/1_dftb+/script.sh
@@ -8,7 +8,6 @@ export OMP_NUM_THREADS=16
 # GENESIS (atdyn) and QM both run with 1 MPI x 16 threads
 #
 export PATH=/path/to/genesis-2.1/bin:$PATH
-mpirun -np 1 -ppn 1 atdyn qmmm_min.inp >& qmmm_min.out
 mpirun -np 1 -ppn 1 atdyn qmmm_nvt.inp >& qmmm_nvt.out
 
 exit 0
